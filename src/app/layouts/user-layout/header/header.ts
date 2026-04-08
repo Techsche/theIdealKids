f967a4b65cef0b85056a1664a723fec322d59dc7
Salini Vijayan <salini@techsche.com>
2026-04-08 11:03:36 +0530

    footer redesign

--- src/app/layouts/user-layout/header/header.ts
+++ src/app/layouts/user-layout/header/header.ts
@@ -3,14 +3,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatMenuModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
@@ -21,6 +19,4 @@ export class Header {
     const menu = document.querySelector('.nav-menu');
     menu?.classList.toggle('active');
   }
-
- 
 }
