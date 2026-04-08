efd0d436e0ebfbf66896a1429c63d6bb15590a68
Salini Vijayan <salini@techsche.com>
2026-04-08 10:07:51 +0530

    footer html chnages

--- src/app/layouts/user-layout/footer/footer.ts
+++ src/app/layouts/user-layout/footer/footer.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, RouterModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
