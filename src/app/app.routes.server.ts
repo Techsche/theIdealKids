aa7485b17733433d330e0dbfbf1f79f79279d47c
Salini Vijayan <salini@techsche.com>
2026-08-05 11:15:43 +0530

    current info page issues fixed

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'child/edit/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'student-attendance/:registerId',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'student-competition/:eventId/:studentId/:registrationNo',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
